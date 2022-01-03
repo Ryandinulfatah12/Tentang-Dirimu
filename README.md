457e162d16eeccc6d3222f747f73149d82a076a4
Ryan Dinul Fatah <ryandfbasketball12@gmail.com>
2022-01-04 00:08:01 +0700

    update card, form, and other

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tentang-dirimu
+
 Aplikasi Simple yang akan bisa mengetahui zodiakmu
+
+<p align="center">
+<img align="center" src="http://ForTheBadge.com/images/badges/built-with-love.svg"> <img align="center" src="http://ForTheBadge.com/images/badges/uses-html.svg"> <img align="center" src="https://forthebadge.com/images/badges/uses-css.svg">  <img align="center" src="http://ForTheBadge.com/images/badges/makes-people-smile.svg"> <img align="center" src="http://ForTheBadge.com/images/badges/built-by-developers.svg">
+</p>
--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-$(document).ready(function(){
-    $( "#yourDate" ).datepicker({
-        dateFormat: 'dd-mm-yy',
-        showAnim: 'slideDown',
-        changeMonth: true,
-        changeYear: true
-    });
+$(document).ready(function () {
+  $("#inputDate").flatpickr({
+    dateFormat: "d-m-Y",
+  });
 });
 
 // Search Data by Name & date function
 function checkMyName() {
-    $('#app').html('');
-    $.ajax({
-        url: 'https://script.google.com/macros/exec',
-        type: 'GET',
-        dataType: 'json',
-        data: {
-            'service': 'AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w',
-            'nama': $('#yourName').val(),
-            'tanggal': $('#yourDate').val()
-        },
-        success: (result) => {
-            if(result.status == "success"){
-                let query = result.data;
-                $('#app').append(`
+  $("#app").html("");
+  $.ajax({
+    url: "https://script.google.com/macros/exec",
+    type: "GET",
+    dataType: "json",
+    data: {
+      service: "AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w",
+      nama: $("#inputName").val(),
+      tanggal: $("#inputDate").val(),
+    },
+    success: (result) => {
+      if (result.status == "success") {
+        let query = result.data;
+        $("#app").append(`
                     <img src="assets/undraw_my_documents_ym8x.png">
                     <h2>${query.nama}</h2>
                     <p><strong>Lahir:</strong> ${query.lahir}</p>
@@ -30,16 +27,16 @@ function checkMyName() {
                     <p><strong>Usiamu:</strong> ${query.usia}</p>
                     <p><strong>Zodiakmu:</strong> ${query.zodiak}</p>
                 `);
-                $('#yourDate').val('');
-                $('#yourName').val('');
-            } else {
-                $('#app').html(`<h3>Ada yang salah, coba lagi :(</h3>`);
-            }
-        }
-    });
+        $("#yourDate").val("");
+        $("#yourName").val("");
+      } else {
+        $("#app").html(`<h3>Upps..Ada yang salah, coba lagi :(</h3>`);
+      }
+    },
+  });
 }
 
 // On button clicked
-$('#btn').on('click', function () {
-    checkMyName();
+$("#btn").on("click", function () {
+  checkMyName();
 });
